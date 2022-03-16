28ddecad7a208515cad9c9ff4fe8e758099871f2
Donny <doniwahyu14@gmail.com>
2022-03-17 00:55:45 +0700

    1

---
+++ part 1/task/task1.js
@@ -0,0 +1,50 @@
+const biodata = {
+    name : "Donny",
+    age : 23,
+    hobbies : ["gaming", "watch anime", "watch movies", "menghalu"],
+    IsMarried : false,
+    schoolList : [ 
+        {            
+            name : "SDN Tadika Mesra 2",
+            yearIn : 2002,
+            yearOut : 2011,
+            major : null 
+        }, {
+            name : "SMPN 1 Tadika Mesra",
+            yearIn : 2011,
+            yearOut : 2014,
+            major : null
+        }, {
+            name : "SMAN Tadika Mesra",
+            yearIn : 2014,
+            yearOut : 2017,
+            major : "Sains"
+        }, {
+            name : "Universitas Sanata Dharma",
+            yearIn : 2017,
+            yearOut : "-",
+            major : "Informatika"
+        }
+    ],
+    skill : [
+        {
+            skillName : "javascript",
+            level : "beginner"
+        }, {
+            skillName : "matlab",
+            level : "beginner"
+        }, {
+            skillName : "php",
+            level : "beginner"
+        }, {
+            skillName : "html",
+            level : "beginner"
+        }, {
+            skillName : "css",
+            level : "beginner"
+        }
+    ],
+    interestInCoding : true
+}
+
+console.log(biodata)
